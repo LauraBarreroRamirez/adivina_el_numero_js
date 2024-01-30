8163d8cb14005a722245747fae0badea1dde2ffe
LauraBarreroRamirez <laurabarrero295@gmail.com>
2024-01-29 19:29:35 -0500

    while para ciclo

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 //variables
-let numeroSecreto = 2;
-let numeroUsuario = prompt ("Me indicas un numero por favor");
+let numeroSecreto = 4;
+let numeroUsuario = 0;
+let intentos = 1;
 
-console.log(numeroUsuario)
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = prompt ("Me indicas un numero entre 1 y 10 por favor");
 
     //Este codigo realiza la comparacion
     if (numeroUsuario == numeroSecreto) {
-        alert (`Acertaste, el numero es: ${numeroSecreto}.`)
+        alert (`Acertaste, el numero es: ${numeroSecreto}. Lo lograste en ${intentos}.`)
     } else if (numeroUsuario < numeroSecreto) {
             alert ("el numero secreto es mayor")
     } else {
         alert ("el numero secreto es menor")
     } 
+    //contador de intentos
+    intentos = intentos + 1;
+} 
