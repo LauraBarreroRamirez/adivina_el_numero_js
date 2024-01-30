278c2cb457829c740acdf2114e761ecb4faed2b4
LauraBarreroRamirez <laurabarrero295@gmail.com>
2024-01-29 19:00:45 -0500

    pista con condicional

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ let numeroUsuario = prompt ("Me indicas un numero por favor");
 
 console.log(numeroUsuario)
 
+    //Este codigo realiza la comparacion
+    if (numeroUsuario == numeroSecreto) {
+        alert (`Acertaste, el numero es: ${numeroSecreto}.`)
+    } else if (numeroUsuario < numeroSecreto) {
+            alert ("el numero secreto es mayor")
+    } else {
+        alert ("el numero secreto es menor")
+    } 
